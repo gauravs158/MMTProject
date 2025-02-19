56ada29ffa8540b57f2efed2323d760a94250ab1
Gaurav <gauravs158@gmail.com>
2025-02-19 23:45:35 +0530

    19-02-2025 Night Update

--- MMTProject/src/main/java/homePage/HomePageInit.java
+++ MMTProject/src/main/java/homePage/HomePageInit.java
@@ -2,10 +2,12 @@ package homePage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class HomePageInit {
 
 	private WebDriver driver;
+
 
 	public HomePageInit(WebDriver driver) {
 		// TODO Auto-generated constructor stub
@@ -13,15 +15,19 @@ public class HomePageInit {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+	@Test
 	public void initExecution() {
 		
 		HomePageLocaters hpl = new HomePageLocaters(driver);
 		try {
 			hpl.enterFromCity();
+			Thread.sleep(2000);
 			hpl.enterToCity();
+			Thread.sleep(2000);
 			hpl.enterDateOfTravel();
+			Thread.sleep(2000);
 			hpl.enterTravellerDetails();
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
